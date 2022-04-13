2b2d186d07b350a60233c11b18055211f184c9d9
Kristan Frost <kristan.frost@gmail.com>
2022-04-13 17:07:02 -0500

    Classes part 2 completed

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Book book1  = new Book("Hitchhiker's Guide to the Galaxy", "Douglas Adams");
+        Book book2 = new Book("A Spell got Chameleon", "Piers Anthony");
+        Book book3 = new Book("Bring Me the Head of Prince Charming", "Sheckley & Zelanzy");
+
+//        Book book4  = new Book("Hitchhiker's Guide to the Galaxy", "Douglas Adams");
+//
+//        System.out.println(book1);
+//
+//        System.out.println(book1.equals(book4));
+//        System.out.println(book2.hashCode());
+//        System.out.println(book3.hashCode());
+
+    }
+}
